2c0f5cb55dad34d1ed3cd38208b04993a3fa4bc3
Sayan Roy <55015854+sayanroy058@users.noreply.github.com>
2025-03-26 00:05:29 +0530

    Update backend_api.py

--- backend_api.py
+++ backend_api.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
